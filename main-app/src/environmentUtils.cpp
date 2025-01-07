aa0240d3b6772f0872861142a9da8bbb798b6a7a
Simon Thorell <kontakt@simonthorell.se>
2025-01-07 20:31:27 +0100

    Move C++ core app to new branch

--- main-app/src/environmentUtils.cpp
+++
@@ -1,7 +0,0 @@
-#include "environmentUtils.h"
-#include <cstdlib>
-
-std::string getEnvironmentVariable(const std::string &var) {
-    const char *value = std::getenv(var.c_str());
-    return value ? std::string(value) : "";
-}
